7bd18c425b795465e5f41c3d551f7fbb3a307c99
quyen <quyen2k386@gmail.com>
2024-03-25 15:34:58 +0700

    update

--- config/ConnectDB.js
+++ config/ConnectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.URL_MONGODB + "GeotechStore2")
+  .connect(process.env.URL_MONGODB + "GeotechStore")
   .then(() => console.log("connect successfully ✅ !!!"))
   .catch((err) => console.log("connect failed ❌: ", err));
   
